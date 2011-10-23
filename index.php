963249b54eddaf8d55aeb2665bb5f300dcff2650
BUATHIER Quentin <quentin01@live.fr>
2011-10-23 18:23:11 +0200

    Mise à jour autoload

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $autoloader = new PHPPie\Autoload\Autoloader();
 $autoloader->registerNamespaces(array(
     'PHPPie' => __DIR__.'/vendor/PHPPie',
     'Doctrine' => __DIR__.'/vendor/Doctrine',
+    'Symfony' => __DIR__.'/vendor/Doctrine/Symfony',
 ));
 
 $autoloader->registerPrefixes(array(
