902364a5fa9e62adcbaa27c5f57132c72bcf82d5
root <root@Quentin-PC.(none)>
2011-10-23 18:31:50 +0200

    Modification de la configuration de l'autoload

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require 'vendor/PHPPie/Autoload/Autoloader.php';
 $autoloader = new PHPPie\Autoload\Autoloader();
 
 $autoloader->registerNamespaces(array(
-    'PHPPie'           => __DIR__.'/vendor/PHPPie',
-    'Doctrine'         => __DIR__.'/vendor/Doctrine',
-    'Symfony'          => __DIR__.'/vendor/Doctrine/Symfony',
+    'PHPPie'            => __DIR__.'/vendor/PHPPie',
+    'Doctrine'          => __DIR__.'/vendor/Doctrine',
+    'Symfony'           => __DIR__.'/vendor/Doctrine/Symfony',
 ));
 
 $autoloader->registerPrefixes(array(
-    'Twig_'            => __DIR__.'/vendor/Twig',
+    'Twig_'             => __DIR__.'/vendor/Twig',
 ));
 
 $autoloader->registerNamespaceFallbacks(array(
