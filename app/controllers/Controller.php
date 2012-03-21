0b27d7addc9eddbdc2bc1bb0248f7fe877b8ed9d
BUATHIER Quentin <quentin01@live.fr>
2012-03-21 14:56:16 +0100

    Ajout d'un .htacces manquant

---
+++ app/controllers/Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+class Controller extends \PHPPie\MVC\Controller {
+	
+	public function index()
+	{
+		/*$response = $this->get('http.response');
+		$response->setContent('Test');
+		
+		return $response;*/
+		
+		//return "index";
+		
+		//return $this->get('view', 'Test');
+		
+		$view = $this->get('view'); // Egale à $this->get('view', 'Controller/index');
+		$view->addVariables(array('name' => 'Quentin'));
+		return $view;
+	}
+	
+	public function test()
+	{
+		$view = $this->get('view');
+		$view->addVariables(array('name' => 'Quentin01'));
+		return $view;
+	}
+}
