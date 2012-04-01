1b0a49ce84267fe44fb01865c0256ecd889b1b8e
BUATHIER Quentin <quentin01@live.fr>
2012-04-01 13:13:49 +0200

    Amélioration pour éviter les boucles d'évenements

--- vendor/PHPPie/Event/Listener.php
+++ vendor/PHPPie/Event/Listener.php
@@ -9,22 +9,34 @@ namespace PHPPie\Event;
 
 class Listener {
 	protected $events = array();
+	protected $eventsInUse = array();
 	
 	public function event($name, $parameters)
 	{
 		$methodName = 'on' . ucFirst($name);
 		
+		if(isset($eventsInUse[$name]) && $eventsInUse[$name] === true)
+			return;
+		
 		if(method_exists($this, $methodName))
 		{
+			$eventsInUse[$name] = true;
+			
 			$reflectionMethod = new \ReflectionMethod($this, $methodName);
 			$reflectionMethod->invokeArgs($this, $parameters);
+			
+			$eventsInUse[$name] = false;
 		}
 		else
 		{
 			if(isset($this->events[$name]))
 			{
+				$eventsInUse[$name] = true;
+				
 				$reflectionMethod = new \ReflectionMethod($this, $this->events[$name]);
 				$reflectionMethod->invokeArgs($this, $parameters);
+				
+				$eventsInUse[$name] = false;
 			}
 		}
 	}
